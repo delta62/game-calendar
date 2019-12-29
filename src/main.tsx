8c7576114a1103c96812416ee6323323faf9bc98
Sam Noedel <sam.noedel@gmail.com>
2019-12-28 21:46:26 -0800

    Add schema migration from old data model

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import { h, render } from 'preact'
 import { Provider } from 'react-redux'
 
+
 import store from './store'
 import App from './containers/app'
+import { migrate, needsMigration } from './migrations'
+
+if (needsMigration()) {
+    migrate()
+}
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.bundle.js')
---
+++ src/migrations.ts
@@ -0,0 +1,32 @@
+import { Month } from './models'
+
+export function needsMigration(): boolean {
+    let storageState = localStorage.getItem('redux')
+    return !!storageState && Array.isArray(JSON.parse(storageState).months)
+}
+
+export function migrate(): void {
+    let storageState = JSON.parse(localStorage.getItem('redux')!).months
+
+    let migratedMonths: Month[] = storageState.map((oldState: any) => ({
+        // Delete "monthAbbr" and "monthName"
+        // Change "gameName" to "game"
+        name: oldState.gameName,
+
+        // Change string dates to numbers
+        startDate: oldState.startDate && new Date(oldState.startDate).getTime(),
+        // Change "completeDate" to "finishDate"
+        finishDate: oldState.completeDate && new Date(oldState.completeDate).getTime(),
+        // Change "hundredPercentDate" to "completeDate"
+        completeDate: oldState.hundredPercentDate && new Date(oldState.hundredPercentDate).getTime(),
+    }))
+
+    // Nest months under 2020
+    let migratedState = {
+        months: {
+            2020: migratedMonths
+        }
+    }
+
+    localStorage.setItem('redux', JSON.stringify(migratedState))
+}
