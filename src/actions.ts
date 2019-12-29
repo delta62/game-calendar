f2b557a515faea7af343c9026121ec0d9dadd194
Sam Noedel <sam.noedel@gmail.com>
2019-12-28 20:33:14 -0800

    Add support for multiple years

--- src/actions.ts
+++ src/actions.ts
@@ -1,23 +1,43 @@
 import { Action } from 'redux'
 
+export const monthUpdated = (year: number, month: number, game: string): MonthGameUpdateAction => (
+    { type: 'MONTH_UPDATED',  year, month, game }
+)
+
+export const startToggled = (year: number, month: number): StartToggledAction => (
+    { type: 'START_TOGGLED', year, month, time: new Date().getTime() }
+)
+
+export const finishToggled = (year: number, month: number): FinishToggledAction => (
+    { type: 'FINISH_TOGGLED', year, month, time: new Date().getTime() }
+)
+
+export const completeToggled = (year: number, month: number): CompleteToggledAction => (
+    { type: 'COMPLETE_TOGGLED', year, month, time: new Date().getTime() }
+)
+
 interface MonthGameUpdateAction extends Action<'MONTH_UPDATED'> {
-    idx: number
-    val: string
+    year: number
+    month: number
+    game: string
 }
 
 interface StartToggledAction extends Action<'START_TOGGLED'> {
-    idx: number
-    time: Date
+    year: number
+    month: number
+    time: number
 }
 
 interface FinishToggledAction extends Action<'FINISH_TOGGLED'> {
-    idx: number
-    time: Date
+    year: number
+    month: number
+    time: number
 }
 
 interface CompleteToggledAction extends Action<'COMPLETE_TOGGLED'> {
-    idx: number
-    time: Date
+    year: number
+    month: number
+    time: number
 }
 
 type AppAction =
