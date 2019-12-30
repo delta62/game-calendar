5e4f6943673ee037a69ab3938392097bd1e06b72
Sam Noedel <sam.noedel@gmail.com>
2019-12-29 21:39:17 -0800

    Fix some multi-scrolling issues

--- src/components/swipeable.tsx
+++ src/components/swipeable.tsx
@@ -12,6 +12,8 @@ export interface Props {
 export interface State {
     diffX: number
     startX: number
+    startY: number
+    lockY: boolean
 }
 
 export default class Swipeable extends Component<Props, State> {
@@ -19,7 +21,15 @@ export default class Swipeable extends Component<Props, State> {
 
     constructor(props: Props) {
         super(props)
+
         this.content = createRef()
+        this.state = {
+            diffX: 0,
+            lockY: false,
+            startX: 0,
+            startY: 0,
+        }
+
         document.addEventListener('touchstart', this.onTouchStarted)
     }
 
@@ -46,25 +56,49 @@ export default class Swipeable extends Component<Props, State> {
         return nextProps.index !== this.props.index
     }
 
+    public componentWillUnmount() {
+        document.removeEventListener('touchstart', this.onTouchStarted)
+    }
+
     private onTouchStarted = (event: TouchEvent) => {
         if (event.touches.length !== 1 || !this.content.current) {
             return
         }
 
-        let startX = event.touches[0].clientX
+        let { clientX, clientY } = event.touches[0]
         this.content.current.style.transition = ''
-        this.setState({ startX, diffX: 0 })
+        this.setState({ startY: clientY, startX: clientX, diffX: 0 })
 
-        document.addEventListener('touchmove', this.onTouchMove)
+        document.addEventListener('touchmove', this.onTouchMove, { passive: false })
         document.addEventListener('touchend', this.onTouchEnd)
         document.addEventListener('touchcancel', this.onTouchCancel)
     }
 
     private onTouchMove = (event: TouchEvent) => {
-        let x = event.touches[0].clientX
-        let diffX = x - this.state.startX
-        this.setState({ diffX })
-        this.content.current!.style.transform = `translateX(${diffX}px)`
+        let { clientX, clientY } = event.touches[0]
+        let diffX = clientX - this.state.startX
+        let diffY = clientY - this.state.startY
+        if (this.state.lockY) {
+            event.preventDefault()
+            // Compute X position
+            this.setState({ diffX })
+            this.content.current!.style.transform = `translateX(${diffX}px)`
+        } else {
+            if (Math.abs(diffX) > Math.abs(diffY)) {
+                // Lock Y
+                event.preventDefault()
+                this.setState({ lockY: true })
+            } else {
+                // Lock X. Just remove the event listeners.
+                this.removeListeners()
+            }
+        }
+    }
+
+    private removeListeners() {
+        document.removeEventListener('touchmove', this.onTouchMove)
+        document.removeEventListener('touchend', this.onTouchEnd)
+        document.removeEventListener('touchcancel', this.onTouchCancel)
     }
 
     private onTouchEnd = () => {
@@ -91,15 +125,13 @@ export default class Swipeable extends Component<Props, State> {
             content.style.transform = 'translateX(0)'
         }
 
-        document.removeEventListener('touchmove', this.onTouchMove)
-        document.addEventListener('touchend', this.onTouchEnd)
-        document.addEventListener('touchcancel', this.onTouchEnd)
+        this.setState({ lockY: false })
+        this.removeListeners()
     }
 
     private onTouchCancel = () => {
-        document.removeEventListener('touchmove', this.onTouchMove)
-        document.addEventListener('touchend', this.onTouchEnd)
-        document.addEventListener('touchcancel', this.onTouchEnd)
+        this.setState({ lockY: false })
+        this.removeListeners()
         this.content.current!.style.transform = 'translateX(0)'
     }
 }
