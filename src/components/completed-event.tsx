fc4944567562a456a1c615193d6031397c35915c
Sam Noedel <sam.noedel@gmail.com>
2021-01-01 14:42:25 -0800

    Re-add forward/back buttons to timeline

--- src/components/completed-event.tsx
+++ src/components/completed-event.tsx
@@ -1,7 +1,27 @@
+import { format } from "date-fns"
+
+import Button from './button'
+
 export interface Props {
+  onCompleteGame(): void
+  onUncompleteGame(): void
   time?: number
 }
 
-export default ({ }: Props) => (
-  <h1>Completed</h1>
-)
+export default ({ onCompleteGame, onUncompleteGame, time }: Props) => {
+  if (time) {
+    return (
+      <>
+        <p>100% complete on {format(time, 'LLL do yyyy')}</p>
+        <Button text="Mark as incomplete" onClick={onUncompleteGame} />
+      </>
+    )
+  }
+
+  return (
+    <>
+      <p>Not 100% completed yet</p>
+      <Button text="Mark as 100% complete" onClick={onCompleteGame} />
+    </>
+  )
+}
--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,23 +1,24 @@
 import classnames from 'classnames'
 
 import { Event } from '../models'
-import StartedEvent from './started-event'
-import FinishedEvent from './finished-event'
-import CompletedEvent from './completed-event'
+import StartedEvent from '../containers/started-event'
+import FinishedEvent from '../containers/finished-event'
+import CompletedEvent from '../containers/completed-event'
 import './timeline.scss'
 
 export interface Props {
   events: Event[]
+  game: number
 }
 
-let Timeline = ({ events }: Props) => (
+let Timeline = ({ events, game }: Props) => (
   <div class="timeline">
     {events.map(event => (
       <>
-        <div class={classnames('event', event.type)}>
-          {event.type === 'started' && <StartedEvent time={event.time} />}
-          {event.type === 'finished' && <FinishedEvent time={event.time} />}
-          {event.type === 'completed' && <CompletedEvent time={event.time} />}
+        <div class={classnames('event', { finished: !!event.time })}>
+          {event.type === 'started' && <StartedEvent game={game} time={event.time} />}
+          {event.type === 'finished' && <FinishedEvent game={game} time={event.time} />}
+          {event.type === 'completed' && <CompletedEvent game={game} time={event.time} />}
         </div>
         <span class="line"></span>
       </>
