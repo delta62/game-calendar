78861e4d92deba21b75885401c3d42bee8b7154a
Sam Noedel <sam.noedel@gmail.com>
2021-01-02 13:13:06 -0800

    cleanup

--- packages/game-calendar/build/webpack.base.js
+++ packages/game-calendar/build/webpack.base.js
@@ -3,51 +3,46 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const path = require('path')
 const pkg = require('../package.json')
-const webpack = require('webpack')
 
 module.exports = {
-    entry: {
-        app:              './src/main.tsx',
-        // 'service-worker': './src/service-worker.ts'
+  entry: {
+    app: './src/main.tsx',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+      },
+    ]
+  },
+  resolve: {
+    alias: {
+      react:       'preact/compat',
+      'react-dom': 'preact/compat',
     },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, '../dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-            },
-        ]
-    },
-    resolve: {
-        alias: {
-            react:       'preact/compat',
-            'react-dom': 'preact/compat',
-        },
-        extensions: [ '.js', '.tsx', '.ts' ],
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new CopyPlugin({
-            patterns: [
-                { from: './static/index.html',      to: 'index.html'        },
-                { from: './static/manifest.json',   to: 'manifest.json'     },
-                { from: './static/img/favicon.png', to: './img/favicon.png' },
-                { from: './static/img/logo.png',    to: './img/logo.png'    },
-                { from: './static/img/logo512.png', to: './img/logo512.png' },
-            ],
-        }),
-        new webpack.DefinePlugin({
-            __VERSION__: JSON.stringify(pkg.version),
-        }),
-        new BundleAnalyzerPlugin({
-            analyzerMode:   'static',
-            reportFilename: 'report.html',
-            defaultSizes:   'parsed',
-            openAnalyzer:   false
-        }),
-    ],
+    extensions: [ '.js', '.tsx', '.ts' ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: './static/index.html',      to: 'index.html'        },
+        { from: './static/manifest.json',   to: 'manifest.json'     },
+        { from: './static/img/favicon.png', to: './img/favicon.png' },
+        { from: './static/img/logo.png',    to: './img/logo.png'    },
+        { from: './static/img/logo512.png', to: './img/logo512.png' },
+      ],
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode:   'static',
+      reportFilename: 'report.html',
+      defaultSizes:   'parsed',
+      openAnalyzer:   false
+    }),
+  ],
 }
--- packages/game-calendar/build/webpack.prod.js
+++ packages/game-calendar/build/webpack.prod.js
@@ -1,27 +1,18 @@
 const config = require('./webpack.base')
 const { merge } = require('webpack-merge')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-// const TerserJSPlugin = require('terser-webpack-plugin')
-// const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = merge(config, {
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
-            }
-        ]
-    },
-    // optimization: {
-    //     usedExports: true,
-    //     minimizer: [
-    //         new TerserJSPlugin(),
-    //         new OptimizeCSSAssetsPlugin()
-    //     ]
-    // },
-    plugins: [
-        new MiniCssExtractPlugin()
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+      }
     ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin()
+  ]
 })
