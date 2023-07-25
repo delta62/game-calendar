e718f8b1f1b52af0c68503b5bda8d0f735a74307
Sam Noedel <sam.noedel@gmail.com>
2023-07-24 20:00:09 -0700

    Disband monorepo

--- packages/micro-router/test/test-globals.d.ts
+++
@@ -1,6 +0,0 @@
-// As this file isn't in the "include": [] section of tsconfig.json, editors
-// have trouble resolving definitions in node_modules. We can keep editors happy
-// while also skipping test compilation during incremental builds by pulling
-// the defs in here.
-
-/// <reference path="../node_modules/@types/jest/index.d.ts" />
